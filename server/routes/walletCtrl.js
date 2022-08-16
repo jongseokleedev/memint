3abe2f35a31cd96a6ed55718f07d3c6b66895023
js_lee <jongsuklee45@gmail.com>
2022-08-16 14:18:48 +0900

    feat: trade klay and lcn

--- server/routes/walletCtrl.js
+++ server/routes/walletCtrl.js
@@ -1,20 +1,27 @@
 require("dotenv").config();
-const Web3 = require("web3");
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const abi = require("../contracts/ERC20/ERC20abi");
-const contractAddress = require("../contracts/ERC20/ERC20address");
-const { SERVER_PRIVATEKEY, SERVER_ADDRESS, ETH_NETWORK } = process.env;
-const provider = new HDWalletProvider(SERVER_PRIVATEKEY, ETH_NETWORK);
-const web3 = new Web3(provider);
-const { toWei, fromWei } = web3.utils;
-const myContract = new web3.eth.Contract(abi, contractAddress);
+const Caver = require("caver-js");
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
+const abi = require("../contracts/KIP7/KIP7abi");
+const contractAddress = require("../contracts/KIP7/KIP7address");
+const byteCode = require("../contracts/KIP7/KIP7bin");
+const { SERVER_PRIVATEKEY, SERVER_ADDRESS, BAOBAB_NETWORK } = process.env;
+// const provider = new HDWalletProvider(SERVER_PRIVATEKEY, BAOBAB_NETWORK);
+const provider = new Caver(BAOBAB_NETWORK);
+// const provider = new Caver.providers.HttpProvider(BAOBAB_NETWORK);
+const caver = new Caver(provider);
+// var caver = new Caver();
+// caver.setProvider(new Caver.providers.HttpProvider(BAOBAB_NETWORK));
+const { convertToPeb, convertFromPeb } = caver.utils;
+const myContract = new caver.contract(abi, contractAddress);
+// myContract.setProvider(caver.currentProvider);
 const app = require("../app");
 const firestore = require("firebase-admin/firestore");
 // 원래는 userUID를 통해 계정의 address와 pk를 받아서 tx를 해야하지만, 현재는 firestore가 연결되어있지 않기 때문에 post요청에서 address와 pk를 받아서 진행한다.
 // 토큰 가격은 0.001eth === 1LCN으로 고정한 상태로 진행한다.
-const updateEthAndOnchainToken = async (id, ETHBalance, LCNBalance) => {
+caver.wallet.newKeyring(SERVER_ADDRESS, SERVER_PRIVATEKEY);
+const updateKlayAndOnchainToken = async (id, KlayBalance, LCNBalance) => {
 	await app.db.collection("User").doc(id).update({
-		ethAmount: ETHBalance,
+		klayAmount: KlayBalance,
 		onChainTokenAmount: LCNBalance,
 	});
 };
@@ -32,15 +39,15 @@ const updateOnchainToken = async (id, LCNBalance) => {
 	});
 };
 
-const updateEth = async (id, ETHBalance) => {
-	console.log({ id, ETHBalance });
+const updateKlay = async (id, KlayBalance) => {
+	console.log({ id, KlayBalance });
 	await app.db.collection("User").doc(id).update({
-		ethAmount: ETHBalance,
+		klayAmount: KlayBalance,
 	});
 };
 
-const createEthOnTxLog = async (id, txType, txHash) => {
-	await app.db.collection("User").doc(id).collection("OnchainEthLog").add({
+const createKlayOnTxLog = async (id, txType, txHash) => {
+	await app.db.collection("User").doc(id).collection("OnchainKlayLog").add({
 		txType: txType,
 		txHash: txHash,
 		createdAt: firestore.FieldValue.serverTimestamp(),
@@ -54,31 +61,32 @@ const createLcnOnTxLog = async (id, txType, txHash) => {
 	});
 };
 
-const ETHToLCN = async (req, res) => {
-	const { id, ethAmount } = req.body;
+const KlayToLCN = async (req, res) => {
+	const { id, klayAmount } = req.body;
 	const doc = await app.db.collection("User").doc(id).get();
 	const { address, privateKey } = doc.data();
+	const keyring = new caver.wallet.keyring.singleKeyring(address, privateKey);
 	try {
-		// 먼저 거래 전 사용자의 eth 잔액을 확인한다.
-		const beforeBalance = await web3.eth.getBalance(address);
+		// 먼저 거래 전 사용자의 klay 잔액을 확인한다.
+		const beforeBalance = await caver.rpc.klay.getBalance(address);
+		const check = await caver.klay.accountCreated(address);
+		// user의 지갑에서 server지갑으로 KLAY Amount만큼 transfer하는 함수
+		const getKlayFromUser = async () => {
+			const tx = caver.transaction.valueTransfer.create({
+				from: address,
+				to: SERVER_ADDRESS,
+				gas: 25000,
+				value: convertToPeb(klayAmount.toString(), "KLAY"),
+			});
+			const signedTx = await tx.sign(keyring).then((result) => {
+				return result.getRLPEncoding();
+			});
 
-		// user의 지갑에서 server지갑으로 ETH Amount만큼 transfer하는 함수
-		const getETHFromUser = async () => {
-			const signedTx = await web3.eth.accounts.signTransaction(
-				{
-					to: SERVER_ADDRESS,
-					gas: 2000000,
-					gasPrice: await web3.eth.getGasPrice(),
-					value: toWei(ethAmount.toString()),
-				},
-				privateKey
-			);
-
-			return await web3.eth.sendSignedTransaction(
-				signedTx.rawTransaction,
+			return await caver.rpc.klay.sendRawTransaction(
+				signedTx,
 				async (err, result) => {
 					if (err) return res.status(400).send({ error: err });
-					await createEthOnTxLog(id, "Transfer ETH", result);
+					await createKlayOnTxLog(id, "Transfer KLAY", result);
 					return { status: "success", result };
 				}
 			);
@@ -86,346 +94,177 @@ const ETHToLCN = async (req, res) => {
 
 		// server의 account에서 user의 account로, 알맞은 양의 LCN을 지급하는 함수
 		const sendLCNToUser = async () => {
-			return await myContract.methods
-				.transfer(address, toWei((ethAmount * 1000).toString()))
-				.send({
-					from: SERVER_ADDRESS,
-					gas: 2000000,
-					gasPrice: await web3.eth.getGasPrice(),
-				})
-				.then(async (result) => {
-					await createLcnOnTxLog(id, "Recieve LCN", result.transactionHash);
+			const txInput = caver.abi.encodeFunctionCall(
+				{
+					name: "transfer",
+					type: "function",
+					inputs: [
+						{ type: "address", name: "recipient" },
+						{
+							type: "uint256",
+							name: "amount",
+						},
+					],
+				},
+				[address, convertToPeb((klayAmount * 10).toString())]
+			);
+			const txObject = caver.transaction.smartContractExecution.create({
+				from: SERVER_ADDRESS,
+				to: contractAddress,
+				gas: 50000,
+				input: txInput,
+			});
+			const signedTx = await txObject
+				.sign(caver.wallet.getKeyring(SERVER_ADDRESS))
+				.then((result) => {
+					return result.getRLPEncoding();
 				});
+			return await caver.rpc.klay.sendRawTransaction(
+				signedTx,
+				async (err, result) => {
+					if (err) return res.status(400).send({ error: err });
+					await createLcnOnTxLog(id, "Recieve LCN", result);
+				}
+			);
 		};
 
 		// 이더 받는 함수 실행 후, 이어서 LCN을 보내는 함수 실행
-		getETHFromUser()
-			.then(() => {
-				return sendLCNToUser();
-			})
-			.then(async () => {
-				const ETHBalance = await web3.eth.getBalance(address);
-				const LCNBalance = await myContract.methods.balanceOf(address).call();
-				await updateEthAndOnchainToken(
-					id,
-					Number(fromWei(ETHBalance.toString())),
-					Number(fromWei(LCNBalance.toString()))
-				);
-				res.send({
-					// 아래 나와있는 balance들은 최종 업데이트 된 결과값이다.
-					// 아래 값을 firestore에 저장하고, front에 success 라는 res를 보내면 front에서 정보를 업데이트하여 렌더링하면 될 것 같다.
-					message: "success",
-					ETHBalance: fromWei(ETHBalance.toString()),
-					LCNBalance: fromWei(LCNBalance.toString()),
-				});
-			});
-	} catch (error) {
-		console.error(error);
-	}
-};
-
-const LCNToETH = async (req, res) => {
-	const { id, tokenAmount } = req.body;
-	const doc = await app.db.collection("User").doc(id).get();
-	const { address, privateKey } = doc.data();
-	try {
-		// address에 tokenAmount만큼의 LCN이 있는지 확인한다.
-		// 정확히 비교하려면 아래 beforeBalance에 toWei를 덮어준 후 비교해야 한다. 그렇지만 지금은 일단 생략.
-		const beforeBalance = await myContract.methods.balanceOf(address).call();
-
-		// user의 지갑에서 server지갑으로 tokenAmount만큼 LCN을 transfer하는 함수
-		const getLCNFromUser = async () => {
-			const gasPrice = await web3.eth.getGasPrice();
-			const data = myContract.methods
-				.transfer(SERVER_ADDRESS, toWei(tokenAmount.toString()))
-				.encodeABI();
-
-			const rawTx = {
-				to: contractAddress,
-				gas: 2000000,
-				gasPrice,
-				data,
-			};
-
-			const signedTx = await web3.eth.accounts.signTransaction(
-				rawTx,
-				privateKey
-			);
-
-			return await web3.eth.sendSignedTransaction(
-				signedTx.rawTransaction,
-				async (err, result) => {
-					if (err) return res.status(400).send({ error: err });
-					await createLcnOnTxLog(id, "Transfer LCN", result);
-					return result;
-				}
-			);
-		};
-
-		// server의 account에서 user의 account로, 알맞은 양의 ETH를 지급하는 함수
-		const sendETHToUser = async () => {
-			const gasPrice = await web3.eth.getGasPrice();
-
-			return web3.eth
-				.sendTransaction({
-					from: SERVER_ADDRESS,
-					to: address,
-					gas: 2000000,
-					gasPrice,
-					value: toWei((tokenAmount / 1000).toString()),
+		if (check && beforeBalance > klayAmount) {
+			getKlayFromUser()
+				.then(() => {
+					return sendLCNToUser();
 				})
-				.then(async (result) => {
-					await createEthOnTxLog(id, "Recieve ETH", result.transactionHash);
+				.then(async () => {
+					const KlayBalance = await caver.rpc.klay.getBalance(address);
+					const LCNBalance = await myContract.methods.balanceOf(address).call();
+					await updateKlayAndOnchainToken(
+						id,
+						Number(convertFromPeb(KlayBalance.toString())),
+						Number(convertFromPeb(LCNBalance.toString()))
+					);
+					res.send({
+						// 아래 나와있는 balance들은 최종 업데이트 된 결과값이다.
+						// 아래 값을 firestore에 저장하고, front에 success 라는 res를 보내면 front에서 정보를 업데이트하여 렌더링하면 될 것 같다.
+						message: "success",
+						KlayBalance: convertFromPeb(KlayBalance.toString()),
+						LCNBalance: convertFromPeb(LCNBalance.toString()),
+					});
 				});
-		};
-
-		// LCN 받는 함수 실행 후, 이어서 ETH를 보내는 함수 실행
-		getLCNFromUser()
-			.then(() => {
-				return sendETHToUser();
-			})
-			.then(async () => {
-				const ETHBalance = await web3.eth.getBalance(address);
-				const LCNBalance = await myContract.methods.balanceOf(address).call();
-				await updateEthAndOnchainToken(
-					id,
-					Number(fromWei(ETHBalance.toString())),
-					Number(fromWei(LCNBalance.toString()))
-				);
-				res.send({
-					// 아래 나와있는 balance들은 최종 업데이트 된 결과값이다.
-					// 아래 값을 firestore에 저장하고, front에 success 라는 res를 보내면 front에서 정보를 업데이트하여 렌더링하면 될 것 같다.
-					message: "success",
-					ETHBalance: fromWei(ETHBalance.toString()),
-					LCNBalance: fromWei(LCNBalance.toString()),
-				});
-			});
+		} else {
+			res.status(400).send("There is no account or enough balance");
+		}
 	} catch (error) {
 		console.error(error);
 	}
 };
 
-const toOffChain = async (req, res) => {
-	const { id, tokenAmount, currentTokenAmount } = req.body;
+const LCNToKlay = async (req, res) => {
+	const { id, klayAmount } = req.body;
 	const doc = await app.db.collection("User").doc(id).get();
 	const { address, privateKey } = doc.data();
+	const keyring = new caver.wallet.keyring.singleKeyring(address, privateKey);
 	try {
-		// address에 tokenAmount만큼의 LCN이 있는지 확인한다.
+		// 먼저 거래 전 사용자의 klay 잔액을 확인한다.
 		const beforeBalance = await myContract.methods.balanceOf(address).call();
-
-		// tokenAmount만큼 user의 계정에서 server의 계정으로 LCN을 transfer해주는 함수
+		const check = await caver.klay.accountCreated(address);
+		// user의 지갑에서 server지갑으로 LCN Amount만큼 transfer하는 함수
 		const getLCNFromUser = async () => {
-			const gasPrice = await web3.eth.getGasPrice();
-			const data = myContract.methods
-				.transfer(SERVER_ADDRESS, toWei(tokenAmount.toString()))
-				.encodeABI();
-
-			const rawTx = {
-				to: contractAddress,
-				gas: 2000000,
-				gasPrice,
-				data,
-			};
-
-			const signedTx = await web3.eth.accounts.signTransaction(
-				rawTx,
-				privateKey
-			);
-
-			return await web3.eth.sendSignedTransaction(
-				signedTx.rawTransaction,
-				async (err, result) => {
-					if (err) return res.status(400).send({ error: err });
-					await createLcnOnTxLog(id, "Transfer LCN", result);
-					return result;
-				}
-			);
-		};
-
-		getLCNFromUser().then(async () => {
-			// 아래 나와있는 balance를 firestore db에 업데이트해준다.
-			const LCNBalance = await myContract.methods.balanceOf(address).call();
-			console.log({ currentTokenAmount, tokenAmount, LCNBalance });
-			await updateLCN(
-				id,
-				tokenAmount + currentTokenAmount,
-				Number(fromWei(LCNBalance.toString()))
-			);
-			res.status(200).send({
-				message: "success",
-				LCNBalance: fromWei(LCNBalance.toString()),
-			});
-		});
-	} catch (error) {
-		console.error(error);
-	}
-};
-
-const toOnChain = async (req, res) => {
-	const { id, tokenAmount, currentTokenAmount } = req.body;
-	const doc = await app.db.collection("User").doc(id).get();
-	const { address, privateKey } = doc.data();
-	// firestore에서 user db에 저장되어있는 토큰의 개수가 tokenAmount이상인지 확인한다.
-	// ex) const userInfo = await db.collection('User').doc(id).get()
-	// if(userInfo.data().tokenAmount >= tokenAmount) { ... }
-
-	// server의 account에서 user의 account로, 알맞은 양의 LCN을 지급하는 함수
-	try {
-		const sendLCNToUser = async () => {
-			return await myContract.methods
-				.transfer(address, toWei(tokenAmount.toString()))
-				.send({
-					from: SERVER_ADDRESS,
-					gas: 2000000,
-					gasPrice: await web3.eth.getGasPrice(),
-				})
-				.then(async (result) => {
-					await createLcnOnTxLog(id, "Recieve LCN", result.transactionHash);
-				});
-		};
-
-		sendLCNToUser().then(async () => {
-			const LCNBalance = await myContract.methods.balanceOf(address).call();
-			console.log({ currentTokenAmount, tokenAmount });
-			await updateLCN(
-				id,
-				currentTokenAmount - tokenAmount,
-				Number(fromWei(LCNBalance.toString()))
-			);
-			// const afterTokenAmount = 원래 user document의 tokenAmount - 파라미터로 받은 tokenAmount
-			// firebase에서 onChainTokenAmount에 LCNBalance 값을, tokenAmount에 afterTokenAmount 값을 업데이트해준 후 front에 res를 보내준다.
-			res.status(200).send({
-				message: "success",
-				LCNBalance: fromWei(LCNBalance.toString()),
-			});
-		});
-	} catch (error) {
-		console.log(error);
-	}
-};
-
-const transferETH = async (req, res) => {
-	const { id, ETHAmount, toAddress } = req.body;
-	const doc = await app.db.collection("User").doc(id).get();
-	const { address, privateKey } = doc.data();
-	// ETHAmount보다 beforeBalance의 값이 크거나 같은지 먼저 체크한다.
-
-	try {
-		const beforeBalance = await web3.eth.getBalance(address);
-
-		// user의 address에서 toAddress로 eth를 보내주는 함수
-		const sendETHTo = async () => {
-			const signedTx = await web3.eth.accounts.signTransaction(
+			const txInput = caver.abi.encodeFunctionCall(
 				{
-					to: toAddress,
-					gas: 2000000,
-					gasPrice: await web3.eth.getGasPrice(),
-					value: toWei(ETHAmount.toString()),
+					name: "transfer",
+					type: "function",
+					inputs: [
+						{ type: "address", name: "recipient" },
+						{
+							type: "uint256",
+							name: "amount",
+						},
+					],
 				},
-				privateKey
+				[SERVER_ADDRESS, convertToPeb((klayAmount * 10).toString())]
 			);
-
-			await web3.eth.sendSignedTransaction(
-				signedTx.rawTransaction,
-				async (err, result) => {
-					if (err) return res.status(400).send({ error: err });
-					// console.log({ result });
-					await createEthOnTxLog(id, "Transfer ETH", result);
-					return result;
-				}
-			);
-		};
-
-		sendETHTo().then(async () => {
-			// 아래 afterBalance 값을 user document의 onChainTokenAmount 값에 업데이트해준다.
-			const afterBalance = await web3.eth.getBalance(address);
-			await updateEth(id, Number(fromWei(afterBalance.toString())));
-
-			res.status(200).send({
-				message: "success",
-				balance: fromWei(afterBalance.toString()),
-			});
-		});
-	} catch (error) {
-		res.status(400).send({ error });
-	}
-};
-
-const transferLCN = async (req, res) => {
-	const { id, tokenAmount, toAddress } = req.body;
-	const doc = await app.db.collection("User").doc(id).get();
-	const { address, privateKey } = doc.data();
-	try {
-		// tokenAmount보다 beforeBalance의 값이 크거나 같은지 먼저 체크한다.
-		const beforeBalance = await myContract.methods.balanceOf(address).call();
-
-		// user의 address에서 toAddress로 LCN을 보내주는 함수
-		const sendLCNTo = async () => {
-			const data = await myContract.methods
-				.transfer(toAddress, toWei(tokenAmount.toString()))
-				.encodeABI();
-
-			const rawTx = {
+			const txObject = caver.transaction.smartContractExecution.create({
+				from: address,
 				to: contractAddress,
-				gas: 2000000,
-				gasPrice: await web3.eth.getGasPrice(),
-				data,
-			};
-
-			const signedTx = await web3.eth.accounts.signTransaction(
-				rawTx,
-				privateKey
-			);
-
-			return await web3.eth.sendSignedTransaction(
-				signedTx.rawTransaction,
+				gas: 50000,
+				input: txInput,
+			});
+			const signedTx = await txObject.sign(keyring).then((result) => {
+				return result.getRLPEncoding();
+			});
+			return await caver.rpc.klay.sendRawTransaction(
+				signedTx,
 				async (err, result) => {
 					if (err) return res.status(400).send({ error: err });
 					await createLcnOnTxLog(id, "Transfer LCN", result);
-					return result;
 				}
 			);
 		};
 
-		sendLCNTo().then(async () => {
-			// 아래 afterBalance 값을 firestore에 업데이트해준다.
-			const afterBalance = await myContract.methods.balanceOf(address).call();
-			await updateOnchainToken(id, Number(fromWei(afterBalance.toString())));
-			res.status(200).send({
-				message: "success",
-				balance: fromWei(afterBalance.toString()),
+		const sendKlayToUser = async () => {
+			const tx = caver.transaction.valueTransfer.create({
+				from: SERVER_ADDRESS,
+				to: address,
+				gas: 25000,
+				value: convertToPeb(klayAmount.toString(), "KLAY"),
 			});
-		});
+			const signedTx = await tx
+				.sign(caver.wallet.getKeyring(SERVER_ADDRESS))
+				.then((result) => {
+					return result.getRLPEncoding();
+				});
+
+			return await caver.rpc.klay.sendRawTransaction(
+				signedTx,
+				async (err, result) => {
+					if (err) return res.status(400).send({ error: err });
+					await createKlayOnTxLog(id, "Recieve KLAY", result);
+					return { status: "success", result };
+				}
+			);
+		};
+
+		// server의 account에서 user의 account로, 알맞은 양의 LCN을 지급하는 함수
+
+		// 이더 받는 함수 실행 후, 이어서 LCN을 보내는 함수 실행
+		if (check && beforeBalance > klayAmount * 10) {
+			getLCNFromUser()
+				.then(() => {
+					return sendKlayToUser();
+				})
+				.then(async () => {
+					const KlayBalance = await caver.rpc.klay.getBalance(address);
+					const LCNBalance = await myContract.methods.balanceOf(address).call();
+					await updateKlayAndOnchainToken(
+						id,
+						Number(convertFromPeb(KlayBalance.toString())),
+						Number(convertFromPeb(LCNBalance.toString()))
+					);
+					res.send({
+						// 아래 나와있는 balance들은 최종 업데이트 된 결과값이다.
+						// 아래 값을 firestore에 저장하고, front에 success 라는 res를 보내면 front에서 정보를 업데이트하여 렌더링하면 될 것 같다.
+						message: "success",
+						KlayBalance: convertFromPeb(KlayBalance.toString()),
+						LCNBalance: convertFromPeb(LCNBalance.toString()),
+					});
+				});
+		} else {
+			res.status(400).send("There is no account or enough balance");
+		}
 	} catch (error) {
 		console.error(error);
 	}
 };
 
-const getBalance = async (req, res) => {
-	const { id, address } = req.body;
-
-	try {
-		const ETHBalance = await web3.eth.getBalance(address);
-		const LCNBalance = await myContract.methods.balanceOf(address).call();
-		await updateEthAndOnchainToken(
-			id,
-			Number(fromWei(ETHBalance.toString())),
-			Number(fromWei(LCNBalance.toString()))
-		);
-
-		res.status(200).send({
-			message: "success",
-			ETHBalance: fromWei(ETHBalance.toString()),
-			LCNBalance: fromWei(LCNBalance.toString()),
-		});
-	} catch (error) {
-		res.status(400).send({ error });
-	}
-};
+const toOffChain = () => {};
+const toOnChain = () => {};
+const transferETH = () => {};
+const transferLCN = () => {};
+const getBalance = () => {};
 
 module.exports = {
-	ETHToLCN,
-	LCNToETH,
+	KlayToLCN,
+	LCNToKlay,
 	toOffChain,
 	toOnChain,
 	transferETH,
